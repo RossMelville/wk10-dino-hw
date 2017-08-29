9d6571f97cbdabcdc6f8b5addc02a334a460e4a2
Ross Melville <rossmelville@hotmail.com>
2017-08-29 17:53:12 +0100

    Added to the park_spec so the park and dinosaurs are created before each test.

---
+++ specs/park_spec.js
@@ -0,0 +1,17 @@
+var assert = require("assert");
+var Park = require("../park.js");
+var Dinosaur = require("../dinosaur.js");
+
+describe( "Dinosaur", function(){
+
+  beforeEach( function() {
+    park = new Park;
+    dinosaur1 = new Dinosaur("Tyrannosaurus", 3);
+    dinosaur2 = new Dinosaur("Velociraptor", 1);
+    dinosaur3 = new Dinosaur("Triceratops", 5);
+  })
+
+  
+
+
+})
