bd504bb2f3b483712f3c1c63f9292bb4a6e8e82e
Ross Melville <rossmelville@hotmail.com>
2017-08-29 18:04:17 +0100

    Added test to check dinosaur can be added to park enclosure

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -16,5 +16,13 @@ describe( "Dinosaur", function(){
     assert.strictEqual(park.enclosure.length, 0);
   })
 
+  it("should be able to add dinosaur to enclosure", function (){
+    park.addDinosaur(dinosaur1);
+    park.addDinosaur(dinosaur2);
+    park.addDinosaur(dinosaur3);
+    console.log("Park", park);
+    assert.strictEqual(park.enclosure.length, 3);
+  })
+
 
 })
