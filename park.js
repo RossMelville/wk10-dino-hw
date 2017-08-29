836656f108909ab9a4cf333b6a8e6b4ebe5944c3
Ross Melville <rossmelville@hotmail.com>
2017-08-29 18:31:21 +0100

    Added function to park.prototype to remove dinosaur by type, TDD test passed.

--- park.js
+++ park.js
@@ -3,7 +3,17 @@ var Park = function(){
 
   Park.prototype.addDinosaur = function(dinosaur){
     this.enclosure.push(dinosaur);
-  }
+  };
+
+  Park.prototype.removeDinosaurType = function(type){
+    for(i = 0; i < this.enclosure.length; i++){
+      if (this.enclosure[i].type === type){
+        this.enclosure.splice(i, 1);
+      }
+    }
+  };
+
+  
 }
 
 
